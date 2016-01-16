a0ccd80bae7cf4bd4bf968a6dc8655411535b802
Arpan Chaudhury <arpanch@thoughtworks.com>
2016-01-16 19:40:43 +0530

    adding title and description to image document model

--- api/app/api/models/ImageDocument.scala
+++ api/app/api/models/ImageDocument.scala
@@ -2,7 +2,7 @@ package api.models
 
 import play.api.libs.json.Json
 
-case class ImageDocument(_id: String, url: String)
+case class ImageDocument(_id: String, url: String, title: String, description: String)
 
 object ImageDocument {
   implicit val format = Json.format[ImageDocument]
