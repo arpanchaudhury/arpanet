a8f780f3629be9e51183bb97bcd4d3398ec275c0
Arpan Chaudhury <arpanch@thoughtworks.com>
2016-02-18 08:36:49 +0530

    enabling ajax data load on search and infinite scroll

--- webapp/app/assets/javascripts/search.js
+++ webapp/app/assets/javascripts/search.js
@@ -1,5 +1,7 @@
 //= require good_reads
-//= require pager
+
+var total_results = 0;
+var page_start = 0;
 
 function initialize_search_toggle_dropdown() {
     $('.search-toggle').click(function () {
@@ -33,17 +35,65 @@ function populate_search_data(data) {
     sections.addClass('hidden');
     search_section.replaceWith(data);
 
-    after_timout(100, function () {
+    var updated_search_section = $('#search-results');
+    total_results = updated_search_section.data('total-results');
+
+    if(total_results == updated_search_section.find('.card').length) {
+        remove_load_more_button();
+        page_start = 0;
+    }
+
+    load_more_button_handler();
+
+    after_timout(200, function () {
         resize_videos();
         resize_slides();
-        disable_pager_buttons();
-
-        recompute_height($('#search-results'));
+        recompute_section_height(updated_search_section);
     });
 }
 
-function recompute_height(search_section) {
-    var recomputed_height = search_section.children().height();
+function append_search_data(data) {
+    var pruned_data = $($.parseHTML(data)).find('.snippet');
+
+    $('.results-container').append(pruned_data);
+
+    var updated_search_section = $('#search-results');
+    total_results = updated_search_section.data('total-results');
+
+    if(total_results == updated_search_section.find('.snippet').length) {
+        remove_load_more_button();
+        page_start = 0;
+    }
+
+    after_timout(200, function () {
+        resize_videos();
+        resize_slides();
+        recompute_section_height(updated_search_section);
+    });
+}
+
+function load_more_button_handler() {
+    $('.loader:not(.disabled)').click(function() {
+        page_start += 1;
+
+        var $form = $('#search-form'),
+            dropdown = $('#search-dropdown'),
+            content = dropdown.attr('value'),
+            search_url = $form.attr('action') + '/' + content,
+            query = $form.find('#search-term').val().trim();
+
+        if (query != '') {
+            $.get(search_url, {query: query, 'page-start': page_start}).done(append_search_data);
+        }
+    })
+}
+
+function remove_load_more_button() {
+    $('.loader').remove();
+}
+
+function recompute_section_height(search_section) {
+    var recomputed_height = search_section.children().height() + 100;
     search_section.height(recomputed_height);
 }
 
--- webapp/app/controllers/search_controller.rb
+++ webapp/app/controllers/search_controller.rb
@@ -3,7 +3,14 @@ class SearchController < ApplicationController
     @content = params[:content]
     query = request.query_parameters['query']
     page_start = request.query_parameters['page-start'] ? request.query_parameters['page-start'] : '0'
-    page_length = request.query_parameters['page-length'] ? request.query_parameters['page-length'] : '8'
+
+    page_length_default = if @content == 'write-ups'
+                            '10'
+                          elsif @content == 'photographs'
+                            '16'
+                          end
+    page_length = request.query_parameters['page-length'] ? request.query_parameters['page-length'] : page_length_default
+
 
     conn = Faraday.new "#{Rails.configuration.x.api.url}/search/#{@content}"
 
@@ -14,7 +21,7 @@ class SearchController < ApplicationController
     end
 
     response = JSON.parse(api_response.body)
-    search_results_count = response['count']
+    search_results_count = response['total']
     @search_results = response['results']
     @pager = {:count => search_results_count, :page_start => page_start, :page_length => page_length}
 
