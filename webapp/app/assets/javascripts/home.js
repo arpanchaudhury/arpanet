e243617693cf450879ccdc99007cd2dec66c7d62
Arpan Chaudhury <arpanch@thoughtworks.com>
2016-03-17 01:30:45 +0530

    displaying photograph in search-results page modal only when it is loaded

--- webapp/app/assets/javascripts/home.js
+++ webapp/app/assets/javascripts/home.js
@@ -1,5 +1,5 @@
 function init_homepage() {
-    hide_spinner_on_image_load_event_handler();
+    hide_spinner_on_profile_picture_load_event_handler();
     profile_image_click_event_handler();
 }
 
@@ -12,7 +12,7 @@ function profile_image_click_event_handler() {
     })
 }
 
-function hide_spinner_on_image_load_event_handler() {
+function hide_spinner_on_profile_picture_load_event_handler() {
     $('#profile-image-modal').find('img').load(function() {
         var modal_content = $(this).closest('.modal-content'),
             loader = modal_content.siblings('.loader');
--- webapp/app/assets/javascripts/search.js
+++ webapp/app/assets/javascripts/search.js
@@ -3,7 +3,7 @@ var page_start = 0;
 function init_search() {
     var search_form = $('#search-form');
     toggle_search_event_handler(search_form);
-    user_event_search_handler(search_form);
+    user_search_event_handler(search_form);
 }
 
 function toggle_search_event_handler(search_form) {
@@ -15,7 +15,7 @@ function toggle_search_event_handler(search_form) {
     })
 }
 
-function user_event_search_handler(search_form) {
+function user_search_event_handler(search_form) {
     search_form.find('.search-query').keydown(function (event) {
         if (event.keyCode == 13) {
             search(search_form)
@@ -24,20 +24,6 @@ function user_event_search_handler(search_form) {
     search_form.find('.search-btn').click(function () {
         search(search_form)
     });
-}
-
-function load_more_event_handler() {
-    $('.loader').click(function () {
-        page_start += 1;
-        var search_form = $('#search-form'),
-            dropdown = search_form.find('.dropdown-toggle'),
-            search_url = search_form.attr('action') + '/' + dropdown.attr('value'),
-            search_term = search_form.find('.search-query').val().trim();
-
-        if (search_term != '') {
-            $.get(search_url, {query: search_term, 'page-start': page_start}).done(append_search_data);
-        }
-    })
 }
 
 function search(search_form) {
@@ -51,11 +37,12 @@ function search(search_form) {
 }
 
 function populate_initial_search_results(data) {
-    var other_sections = $('section:not(.search-results)'),
+    var html_data = $.parseHTML(data),
+        other_sections = $('section:not(.search-results)'),
         search_section = $('section.search-results');
     other_sections.addClass('hidden');
     search_section.empty();
-    search_section.append(data);
+    search_section.append(html_data);
     search_section.removeClass('hidden');
 
     var total_results = search_section.find('.results-container').data('total-results');
@@ -66,6 +53,30 @@ function populate_initial_search_results(data) {
 
     load_more_event_handler();
     photograph_click_event_handler();
+    photograph_view_modal_close_event_handler();
+    hide_spinner_on_searched_photograph_load_event_handler();
+}
+
+function load_more_event_handler() {
+    $('.search-result-loader').click(function () {
+        page_start += 1;
+        var search_form = $('#search-form'),
+            dropdown = search_form.find('.dropdown-toggle'),
+            search_url = search_form.attr('action') + '/' + dropdown.attr('value'),
+            search_term = search_form.find('.search-query').val().trim();
+
+        if (search_term != '') {
+            $.get(search_url, {query: search_term, 'page-start': page_start}).done(append_search_data);
+        }
+    })
+}
+
+function photograph_view_modal_close_event_handler() {
+    var modal = $('#photograph-view-modal');
+    modal.find('.close').click(function () {
+        modal.find('.modal-content').addClass('hidden');
+        modal.find('.loader').removeClass('hidden');
+    })
 }
 
 function append_search_data(data) {
@@ -81,6 +92,11 @@ function append_search_data(data) {
     }
 
     photograph_click_event_handler();
+    hide_spinner_on_searched_photograph_load_event_handler();
+}
+
+function remove_load_more_button() {
+    $('.search-result-loader').remove();
 }
 
 function photograph_click_event_handler() {
@@ -92,6 +108,12 @@ function photograph_click_event_handler() {
     })
 }
 
-function remove_load_more_button() {
-    $('.loader').remove();
+function hide_spinner_on_searched_photograph_load_event_handler() {
+    $('#photograph-view-modal').find('img').load(function () {
+        var modal_content = $(this).closest('.modal-content'),
+            loader = modal_content.siblings('.loader');
+        loader.addClass('hidden');
+        modal_content.removeClass('hidden');
+    })
 }
+
