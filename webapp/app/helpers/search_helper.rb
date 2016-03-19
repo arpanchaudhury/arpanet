0fecf4e23f122b66a893de72b97413e344489dcc
Arpan Chaudhury <arpanch@thoughtworks.com>
2016-03-19 12:26:59 +0530

    showing search-term suggestions on zero search results

---
+++ webapp/app/helpers/search_helper.rb
@@ -0,0 +1,13 @@
+module SearchHelper
+  def get_popular_search_terms(content_type)
+    if content_type == 'write-ups'
+      conn = Faraday.new "#{Rails.configuration.x.api.url}/write-ups/topics"
+      api_response = conn.get
+      JSON.parse(api_response.body).take(10).join(', ')
+    else
+      conn = Faraday.new "#{Rails.configuration.x.api.url}/images/photography/tags"
+      api_response = conn.get
+      JSON.parse(api_response.body).take(10).join(', ')
+    end
+  end
+end
